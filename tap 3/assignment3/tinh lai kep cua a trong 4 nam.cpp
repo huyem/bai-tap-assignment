2bdeb86552f442e9eea65ecca183710dc1502f20
huyem <64858748+huyem@users.noreply.github.com>
2020-05-12 15:55:18 +0700

    Update tinh lai kep cua a trong 4 nam.cpp

--- tap 3/assignment3/tinh lai kep cua a trong 4 nam.cpp
+++ tap 3/assignment3/tinh lai kep cua a trong 4 nam.cpp
@@ -2,11 +2,13 @@
 
 int main(){
 	float a;
+	float r;
 	printf ("nhap so tien a :\n",a);
 	scanf ("%f",&a);
+	printf ("lai xuat mot nam r :\n",r);
+	scanf ("%f",&r);
 	if( a > 0 ){
 		float a1,a2,a3,a4;
-		float r;
 		a1 = a + a*r;
 		a2 = a1 + a1*r;
 		a3 = a2 + a2*r;
