c1afd660fb9d9860a535e74bee8298f97a628bc3
huyem <64858748+huyem@users.noreply.github.com>
2020-06-02 19:55:52 +0700

    pracical1

--- pracical/ex1.cpp
+++ pracical/ex1.cpp
@@ -4,11 +4,12 @@ void Concat(char s1[],char s2[]){
 	strcat(s1,s2);
 }
 int main(){
-	char s1[50],s2[50];
+	char s1[50];
+	char s2[50];
 	printf("nhap chuoi s1:\n");
 	scanf("%s",s1);
 	printf("nhap chuoi s2:\n");
 	scanf("%s",s2);
 	Concat;
-	printf("chuoi 1 sau khi noi: %s\n",s1);
+	printf("chuoi sau khi noi: %s %s\n",s1,s2);
 }
